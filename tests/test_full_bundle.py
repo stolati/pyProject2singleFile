4cef14a27825b7f6552ef0364a2fdea9a0eab001
Mickaël Kerbrat <mickael.kerbrat@gmail.com>
2021-12-26 19:04:39 +0000

    Cleaned test code.

--- tests/test_full_bundle.py
+++ tests/test_full_bundle.py
@@ -3,38 +3,48 @@ import importlib
 import unittest
 import pybundle.main
 from pybundle.stdlib import with_clean_modules, with_clean_path
-import pprint
 from dataclasses import asdict
 from tests.logger import LogEvent
 
+def _get_globals_from_code(code):
+  with with_clean_modules(), with_clean_path():
+    ast = compile(code, "code", "exec")
+    global_env = {}
+    _ = exec(ast, global_env)
 
-import_path = 'tests.single_file_import.main'
+  return global_env
+
+def _get_globals_from_path(lib_path):
+  with with_clean_modules():
+    return vars(importlib.import_module(lib_path))
+
+def _re_class_logs(logs_events):
+    # LogEvent objects are from different types.
+    # Because they were loaded through different methods.
+    return [LogEvent(**asdict(e)) for e in logs_events]
+
+
 
 class TestBundleWorks(unittest.TestCase):
 
-  def test_upper(self):
+  def test_single_file_import(self):
+    self._test_import("tests.single_file_import.main")
+
+  def _test_import(self, import_path):
+    expected = _get_globals_from_path(import_path)['logs_extractions']
 
     code = '\n'.join([
       f'from {import_path} import logs_extractions',
     ])
+    output_content, _ = pybundle.main.generate_single_main(import_path, code_to_execute = code)
+    result = _get_globals_from_code(output_content)['logs_extractions']
 
-    expected = importlib.import_module(import_path).logs_extractions
-
-    output_content, modules = pybundle.main.generate_single_main(import_path, code_to_execute = code)
-    
-    with with_clean_modules(), with_clean_path():
-      ast = compile(output_content, "code", "exec")
-      global_env = {}
-      exec_r = exec(ast, global_env)
-
-    result = global_env['logs_extractions']
-
-    # LogEvent objects are form different types
+    # LogEvent objects are from different types.
+    # Because they were loaded through different methods.
     assert expected[0].__class__ is not result[0].__class__
 
-    expected = [LogEvent(**asdict(e)) for e in expected]
-    result = [LogEvent(**asdict(e)) for e in result]
-    self.assertListEqual(expected, result)
+    self.assertListEqual(_re_class_logs(expected), _re_class_logs(result))
+
 
 
 if __name__ == '__main__':
